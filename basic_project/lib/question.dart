ccb026f452599a8b9788010c74eabdacd5e81bbb
Manoj <manoj.mca2012@hotmail.com>
2020-04-26 15:41:41 +0530

    Update question.dart
    Code added for another Widget.

---
+++ basic_project/lib/question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+class Question extends StatelessWidget {
+  String questionText;
+  Question(this.questionText);
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText);
+  }
+}
